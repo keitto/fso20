171c824e3b785165ebe409ce2429e95fce5a4cd4
Teemu Keränen <teemu.keranen@me.com>
2020-07-22 03:58:51 +0300

    1.3-1.5 with fancy array funs

--- osa1/kurssitiedot/src/index.js
+++ osa1/kurssitiedot/src/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const Header = (props) => (
-  <h1>{props.header}</h1>
+  <h1>{props.course.name}</h1>
 )
 
 const Content = (props) => (
   <div>
-    <Part name={props.part1} num={props.ex1}/>
-    <Part name={props.part2} num={props.ex2}/>
-    <Part name={props.part3} num={props.ex3}/>
+    {props.course.parts.map(part => {
+      return (<li>{part.name} - {part.exercises}</li>)
+    })}
   </div>
 )
 
@@ -18,27 +18,33 @@ const Part = (props) => (
 )
 
 const Total = (props) => (
-  <p>Number of exercises {props.ex1 + props.ex2 + props.ex3}</p>
+  <p>{props.course.parts.reduce((accu, part) => accu + part.exercises,0)}</p>
 )
 
 const App = () => {
-  const course = 'Half Stack application Development'
-  const part1 = 'Fundamentals of React'
-  const ex1 = 10 // # of exercises
-  const part2 = 'Using props to pass data'
-  const ex2 = 7
-  const part3 = 'State of a component'
-  const ex3 = 14
+  const course = {
+    name: 'Half Stack application Development',
+    parts: [
+      {
+        name:       'Fundamentals of React',
+        exercises:  10
+      },
+      {
+        name:       'Using props to pass data',
+        exercises:  7
+      },
+      {
+        name:       'State of a component',
+        exercises:  14
+      }  
+    ]  
+  }
 
   return (
     <div>
-      <Header header={course} />
-      <Content 
-        part1={part1} ex1={ex1}        
-        part2={part2} ex2={ex2}
-        part3={part3} ex3={ex3}
-      />
-      <Total ex1={ex1} ex2={ex2} ex3={ex3}/>
+      <Header course={course} />
+      <Content course={course} />
+      <Total course={course}/>
     </div>
   )
 }
