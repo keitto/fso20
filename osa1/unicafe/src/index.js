0dafaf3e62459051d49b8781fa76da3e922090bd
Teemu Keränen <teemu.keranen@me.com>
2020-07-25 17:48:53 +0300

    1.9 nicer stats

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -7,29 +7,43 @@ const Button = ({text, whatFun, amount}) => {
   )
 }
 
-const Stats = ({good,avg,bad}) => {
+const Stats = ({good,neutral,bad}) => {
+  const total = good+neutral+bad
+  const positivePct = 100*(good / total) || 0
+  const avg = (good-bad)/total || 0
+  let statsContent = (<div>No feedback given</div>)
+
+  if(total > 0) {
+    statsContent = (
+      <div>
+        <p>good: {good}</p>
+        <p>neutral: {neutral}</p>
+        <p>bad: {bad}</p>
+        <p>total: {total} - positive: {positivePct.toFixed(1)}% - avg: {avg.toFixed(2)}</p>
+      </div>
+    )
+  }
+  
   return (
     <div>
       <h1>Stats:</h1>
-      <p>good: {good}</p>
-      <p>average: {avg}</p>
-      <p>bad: {bad}</p>
-    </div>  )
+      {statsContent}
+    </div>  
+  )
 }
-
 
 const App = () => {
   const [good, setGood] = useState(0)
-  const [avg, setAvg] = useState(0)
+  const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
   return (
     <div>
   <h1>Did you like maksakastike?</h1>
   <Button text='good'     whatFun={setGood} amount={good + 1} />
-  <Button text='average'  whatFun={setAvg}  amount={avg + 1} />
+  <Button text='neutral'  whatFun={setNeutral}  amount={neutral + 1} />
   <Button text='bad'      whatFun={setBad}  amount={bad + 1} />
-  <Stats good={good} avg={avg} bad={bad} />
+  <Stats good={good} neutral={neutral} bad={bad} />
 
     </div>
   )
