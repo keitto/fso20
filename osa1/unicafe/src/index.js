b459c9d49ba3400a8a012f4eb435b587690acde2
Teemu Keränen <teemu.keranen@me.com>
2020-07-25 18:41:20 +0300

    1.11 table

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -7,6 +7,11 @@ const Button = ({text, whatFun, amount}) => {
   )
 }
 
+const StatsLine = ({text,value}) => (
+  <tr><td>{text}</td><td>{value}</td></tr>
+)
+
+
 const Stats = ({good,neutral,bad}) => {
   const total = good+neutral+bad
   const positivePct = 100*(good / total) || 0
@@ -16,10 +21,17 @@ const Stats = ({good,neutral,bad}) => {
   if(total > 0) {
     statsContent = (
       <div>
-        <p>good: {good}</p>
-        <p>neutral: {neutral}</p>
-        <p>bad: {bad}</p>
-        <p>total: {total} - positive: {positivePct.toFixed(1)}% - avg: {avg.toFixed(2)}</p>
+        <table>
+          <tbody>
+            <StatsLine text='good' value={good} />
+            <StatsLine text='neutral' value={neutral} />
+            <StatsLine text='bad' value={bad} />
+
+            <StatsLine text='total' value={total} />
+            <StatsLine text='positive' value={positivePct.toFixed(1)+'%'} />
+            <StatsLine text='average' value={avg.toFixed(2)} />
+          </tbody>
+        </table>
       </div>
     )
   }
