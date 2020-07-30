e88a937a4f4edeba45bfae5ac49bfaff4de16832
Teemu Keränen <teemu.keranen@me.com>
2020-07-30 14:15:06 +0300

    2.9 filtering

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,10 +1,12 @@
 import React, {useState} from 'react';
 
-const Contacts = ({persons}) => {
-  return (
+const Contacts = ({persons,filterInput}) =>
+  (
     <table>
       <tbody>
-      {persons.map((person) => 
+      {persons
+      .filter((p)=>p.name.toLowerCase().includes(filterInput.toLowerCase()))
+      .map((person) => 
         <tr key={person.name}>
           <td>{person.name}</td>
           <td>{person.number || ''}</td>
@@ -12,11 +14,10 @@ const Contacts = ({persons}) => {
       </tbody>
     </table>
   )    
-}
 
 const Newform = ({nameInput, nameInputChange, 
-  numberInput, numberInputChange, addButtonClick}) => {
-  return (
+  numberInput, numberInputChange, addButtonClick}) => 
+  (
     <form>
       <div>
         Add new contact: 
@@ -28,22 +29,33 @@ const Newform = ({nameInput, nameInputChange,
       </div>
     </form>
   )
-}
+
+const Filter = ({filterInput,filterInputChange}) =>
+  (
+    <div>
+      <input value={filterInput} onChange={filterInputChange}/>
+    </div>
+  )
 
 const App = () => {
   const [ persons, setPersons] = useState([
     { name: 'Arto Hellas', number: '0401234567' },
-    { name: 'Tero Nuppi', number:'0500987654'}
+    { name: 'Tero Nuppi', number:'0500987654'},
+    { name: 'Arto Paasilinna', number: '0404040404'}
   ]) 
   const [nameInput, setNameInput] = useState('')
   const [numberInput, setNumberInput] = useState('')
+  const [filterInput, setFilterInput] = useState('')
 
   const nameInputChange = (e) => setNameInput(e.target.value)
 
   const numberInputChange = (e) => setNumberInput(e.target.value)
 
+  const filterInputChange = (e) => setFilterInput(e.target.value)
+
   const addButtonClick = (e) => {
     e.preventDefault()
+    if(nameInput.trim() === '') return false // no invisible names
     if(persons.map((i) => i.name).includes(nameInput)) {
       alert(`Name ${nameInput} exists`)
       return false
@@ -55,15 +67,18 @@ const App = () => {
 
   return (
     <div>
-      <h2>Phonebook</h2>
+      <h1>Phonebook</h1>
+      <h2>Add new</h2>
       <Newform nameInput={nameInput}
         nameInputChange={nameInputChange}
         numberInput={numberInput}
         numberInputChange={numberInputChange}
         addButtonClick={addButtonClick}
       />
-      <h2>Contacts</h2>
-      <Contacts persons={persons}/>
+      <h2>Filter</h2>
+      <Filter filterInput={filterInput} filterInputChange={filterInputChange}/>
+      <h2>Contacts{filterInput.length>0?'*':''}</h2>
+      <Contacts persons={persons} filterInput={filterInput}/>
     </div>
   )
 
