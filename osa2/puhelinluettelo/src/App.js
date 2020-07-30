2640a262883da74f1efc457a882d0a9e0b33d849
Teemu Keränen <teemu.keranen@me.com>
2020-07-30 13:06:31 +0300

    2.7 duplicate checker

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -8,12 +8,11 @@ const Contacts = ({persons}) => {
   )    
 }
 
-
 const App = () => {
   const [ persons, setPersons] = useState([
     { name: 'Arto Hellas' }
   ]) 
-  const [nameInput, setNameInput] = useState('name?')
+  const [nameInput, setNameInput] = useState('')
 
 
   const nameInputChange = (e) => {
@@ -22,23 +21,26 @@ const App = () => {
 
   const addButtonClick = (e) => {
     e.preventDefault()
+    if(persons.map((i) => i.name).includes(nameInput)) {
+      alert(`Name ${nameInput} exists`)
+      return false
+    }
     setPersons(persons.concat({name: nameInput}))
     setNameInput('')
   }
-
 
   return (
     <div>
       <h2>Phonebook</h2>
       <form>
         <div>
-          name: <input value={nameInput} onChange={nameInputChange}/>
+          Add new contact: <input value={nameInput} onChange={nameInputChange}/>
         </div>
         <div>
           <button type="submit" onClick={addButtonClick}>add</button>
         </div>
       </form>
-      <h2>Numbers</h2>
+      <h2>Contacts</h2>
       <Contacts persons={persons}/>
     </div>
   )
